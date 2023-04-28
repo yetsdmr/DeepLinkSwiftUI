c6d3e51c73ed37090e8213ba6c718bd3a9c572bd
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-28 17:03:41 +0300

    Added deep link process

---
+++ DeepLinkSwiftUI/View/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  DeepLinkSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 28.04.2023.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
---
+++ DeepLinkSwiftUI/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  DeepLinkSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 28.04.2023.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
