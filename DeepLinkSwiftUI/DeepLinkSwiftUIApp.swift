192da33bfd7f1f6f6c65e44d4963ea6d004ce2ba
Yunus Emre Taşdemir <emre.tasdemir@btc-ag.com.tr>
2023-04-28 09:04:43 +0300

    Initial Commit

---
+++ DeepLinkSwiftUI/DeepLinkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkSwiftUIApp.swift
+//  DeepLinkSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
